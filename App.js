d07bac10b8e1ffddbdb1ca8d37e09bbd39ce4db8
Ashraful Islam <ashrafunit7@gmail.com>
2021-12-29 00:55:30 +0600

    folder architecture, building a simple navigation

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [focus, setFocus] = useState(null);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>{focus}</Text> */}
+      {focus ? <Text>That should be row</Text> : <Text>That should be column</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
   },
 });
